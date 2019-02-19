ca2a99eb300ec912115854f5e4080b529513dbd1
Luca Forzutti <darkartek@gmail.com>
2019-02-19 20:32:23 +0100

    Update urls.py

--- ffxivitashade/urls.py
+++ ffxivitashade/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('presets/', include('presets.urls')),
     path('account/', include('accounts.urls')),
-] static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
