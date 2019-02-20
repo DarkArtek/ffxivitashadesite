06b9407691cf5fb89b0adebe5b3d28cba858c78c
Artek <l.forzutti@finalfantasyxivitalia.it>
2019-02-20 13:10:00 +0100

    Storages

--- ffxivitashade/settings.py
+++ ffxivitashade/settings.py
@@ -40,12 +40,12 @@ INSTALLED_APPS = [
     # http://whitenoise.evans.io/en/stable/django.html#using-whitenoise-in-development
     # 'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
+    'storages',
     'django.contrib.humanize',
     'presets',
     'accounts',
     'pages',
     'creators',
-    'storages',
 ]
 
 MIDDLEWARE = [
@@ -129,10 +129,18 @@ ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+AWS_ACCESS_KEY_ID = "AKIAJHS66XMSU5DILHAA"
+AWS_SECRET_ACCESS_KEY = "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"
+AWS_STORAGE_BUCKET_NAME = "shader"
+AWS_DEFAULT_ACL = "public-read"
+AWS_S3_ENCRYPTION = True
+AWS_S3_REGION_NAME = "eu-central-1"
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
     os.path.join(PROJECT_ROOT, 'static'),
