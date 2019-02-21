25ccfb6348e382dca26ef9a4135afb157ace9be9
Luca Forzutti <darkartek@gmail.com>
2019-02-21 08:59:47 +0100

    url reverted to default

--- allauth/account/urls.py
+++ allauth/account/urls.py
@@ -1,34 +1,34 @@
-from django.urls import re_path
+from django.conf.urls import url
 
 from . import views
 
 
 urlpatterns = [
-    re_path(r"^signup/$", views.signup, name="account_signup"),
-    re_path(r"^login/$", views.login, name="account_login"),
-    re_path(r"^logout/$", views.logout, name="account_logout"),
+    url(r"^signup/$", views.signup, name="account_signup"),
+    url(r"^login/$", views.login, name="account_login"),
+    url(r"^logout/$", views.logout, name="account_logout"),
 
-    re_path(r"^password/change/$", views.password_change,
+    url(r"^password/change/$", views.password_change,
         name="account_change_password"),
-    re_path(r"^password/set/$", views.password_set, name="account_set_password"),
+    url(r"^password/set/$", views.password_set, name="account_set_password"),
 
-    re_path(r"^inactive/$", views.account_inactive, name="account_inactive"),
+    url(r"^inactive/$", views.account_inactive, name="account_inactive"),
 
     # E-mail
-    re_path(r"^email/$", views.email, name="account_email"),
-    re_path(r"^confirm-email/$", views.email_verification_sent,
+    url(r"^email/$", views.email, name="account_email"),
+    url(r"^confirm-email/$", views.email_verification_sent,
         name="account_email_verification_sent"),
-    re_path(r"^confirm-email/(?P<key>[-:\w]+)/$", views.confirm_email,
+    url(r"^confirm-email/(?P<key>[-:\w]+)/$", views.confirm_email,
         name="account_confirm_email"),
 
     # password reset
-    re_path(r"^password/reset/$", views.password_reset,
+    url(r"^password/reset/$", views.password_reset,
         name="account_reset_password"),
-    re_path(r"^password/reset/done/$", views.password_reset_done,
+    url(r"^password/reset/done/$", views.password_reset_done,
         name="account_reset_password_done"),
-    re_path(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+    url(r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
         views.password_reset_from_key,
         name="account_reset_password_from_key"),
-    re_path(r"^password/reset/key/done/$", views.password_reset_from_key_done,
+    url(r"^password/reset/key/done/$", views.password_reset_from_key_done,
         name="account_reset_password_from_key_done"),
 ]
